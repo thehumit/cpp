6fee8fa10bece5017fdfff366d42f9c98b3ad284
Tenebrous Quintana <tquintan@mi-h3.msk.21-school.ru>
2021-09-12 12:40:35 +0300

    day05

--- day05/ex00/Bureaucrat.hpp
+++ day05/ex00/Bureaucrat.hpp
@@ -35,68 +35,6 @@ public:
     void decrement();
 };
 
-Bureaucrat::Bureaucrat(std::string name, int points)
-    : _name(name), _points(points)
-{
-    if (points < 1)
-        throw Bureaucrat::GradeTooHighException();
-    if (points > 150)
-        throw Bureaucrat::GradeTooLowException();
-    
-}
-
-Bureaucrat&	Bureaucrat::operator= (const Bureaucrat &cp)
-{
-    if (this == &cp)
-        return (*this);
-    this->_name = cp._name;
-    this->_points = cp._points;
-	return (*this);
-}
-
-Bureaucrat::Bureaucrat(const Bureaucrat &cp)
-{
-    (*this) = cp;
-}
-
-Bureaucrat::~Bureaucrat()
-{
-    std::cout << "Bureaucrat destructor called." << std::endl;
-}
-
-std::string const &Bureaucrat::getName() const
-{
-    return this->_name;
-}
-
-int const &Bureaucrat::getPoints() const
-{
-    return this->_points;
-}
-
-void Bureaucrat::increment()
-{
-    if (this->_points - 1 < 1)
-    {
-        throw Bureaucrat::GradeTooHighException();
-    }
-    this->_points--;
-}
-
-void Bureaucrat::decrement()
-{
-    if (this->_points + 1 > 150)
-    {
-        throw Bureaucrat::GradeTooLowException();
-    }
-    this->_points++;
-}
-
-std::ostream& operator<< (std::ostream &out, const Bureaucrat &bureaucrat)
-{
-    // Поскольку operator<< является другом класса Bureaucrat, то мы имеем прямой доступ к членам Bureaucrat
-    out << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getPoints() << ".";
-    return out;
-}
+std::ostream& operator<< (std::ostream &out, const Bureaucrat &bureaucrat);
 
 #endif
